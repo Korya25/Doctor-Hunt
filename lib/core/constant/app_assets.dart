395a1e71961cf07080c3d0c61a61f48e8f445740
Korya25 <enthrj3@gmail.com>
2025-06-07 01:16:25 +0300

    Add new SVG icons to AppIconsSvgs class

--- lib/core/constant/app_assets.dart
+++ lib/core/constant/app_assets.dart
@@ -13,4 +13,12 @@ class AppImages {
 
 class AppIconsSvgs {
   static const String appIcon = 'assets/icons/app_icon.svg';
+  static const String arrowIos = 'assets/icons/arrow_ios.svg';
+  static const String star = 'assets/icons/star.svg';
+  static const String favorite = 'assets/icons/favorite.svg';
+  static const String nonFavorite = 'assets/icons/non_favorite.svg';
+  static const String sprocket = 'assets/icons/sprocket.svg';
+  static const String heart = 'assets/icons/heart.svg';
+  static const String eye = 'assets/icons/eye.svg';
+  static const String body = 'assets/icons/body.svg';
 }
